70cbf8ec4f1b1d8ac4481d63322a01d4d7515597
martin <344321773@qq.com>
2018-09-27 16:42:21 +0800

    进度条功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,9 +1,13 @@
+// NProgress.start();
+//   setTimeout(function(){
+//  NProgress.stop();
+//   },1000)
+// 需求变了 变成发送ajax 的时候开始进度条 ajax 成功后结束进度条
 $(document).ajaxStart(function(){
   NProgress.start();
 });
 $(document).ajaxStop(function(){
-  // 模拟网络延迟
-  setTimeout(function(){
-    NProgress.done();
-  },500)
+  NProgress.done();
 })
+
+ 
