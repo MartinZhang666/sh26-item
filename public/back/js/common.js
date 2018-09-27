e808cbdfef39d542469b36207045da47d05eda88
martin <344321773@qq.com>
2018-09-27 20:10:05 +0800

    首页模态框成功

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -10,4 +10,29 @@ $(document).ajaxStop(function(){
   NProgress.done();
 })
 
+
+
+//  公共
+// 1.二级菜单切换功能
+// 2.左侧菜单栏切换
+// 3.退出功能
+$(function(){
+ // 1.二级菜单切换功能
+ $('.category').click(function(){
+   $('.lt_aside .child').stop().slideToggle();
+ })
+// 2.左侧菜单栏切换
+ $('.icon_menu').click(function(){
+   $('.lt_aside').toggleClass('hidemenu');
+   $('.lt_topbar').toggleClass('hidemenu');
+   $('.lt_main').toggleClass('hidemenu');
+
+ })
  
+ $('.icon_logout').click(function(){
+   $('#logoutModal').modal('show');
+ })
+
+
+
+})
